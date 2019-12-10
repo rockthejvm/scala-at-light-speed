43415081e431396ff521f89f2a2a676131631f09
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2019-12-10 22:15:33 +0200

    playground comment

--- src/main/scala/com/rockthejvm/Playground.scala
+++ src/main/scala/com/rockthejvm/Playground.scala
@@ -1,5 +1,13 @@
 package com.rockthejvm
 
+/**
+  * This simple application is an invitation to you to play around with any Scala code you like!
+  * (not that you'd not be able to create your own application, mind you.)
+  *
+  * So get creative - test the code that we write in the videos and try new things!
+  *
+  * Daniel @ Rock the JVM
+  */
 object Playground extends App {
   println("I love Scala!")
 }

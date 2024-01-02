4035e241e0afa8c34eca4866b2ac7ef5c5d5831b
Kavya P S <kavya.ps095@gmail.com>
2024-01-02 16:55:13 +0530

    Notes - Pattern Matching

--- src/main/scala/com/rockthejvm/PatternMatching.scala
+++ src/main/scala/com/rockthejvm/PatternMatching.scala
@@ -8,33 +8,40 @@ object PatternMatching extends App {
     case 1 => "first"
     case 2 => "second"
     case 3 => "third"
-    case _ => anInteger + "th"
+    case _ => anInteger + "th" // case _ === default|else
   }
-  // PM is an EXPRESSION
+  // Pattern Matching is an EXPRESSION
 
   // Case class decomposition
-  case class Person(name: String, age: Int)
-  val bob = Person("Bob", 43) // Person.apply("Bob", 43)
+  case class Person(name: String, age: Int) // similar to data object in Kotlin
+
+  val bob = Person("Bob", 43) // === Person.apply("Bob", 43)
 
   val personGreeting = bob match {
     case Person(n, a) => s"Hi, my name is $n and I am $a years old."
+    //if bob matches the structure of Person(n,a) ^^
     case _ => "Something else"
   }
+  //  !!! Pattern Matching is only available for Case classes for now !!!
+  //    (need to do a lot of changes to use with normal classes)
+
 
   // deconstructing tuples
   val aTuple = ("Bon Jovi", "Rock")
   val bandDescription = aTuple match {
     case (band, genre) => s"$band belongs to the genre $genre"
+    // ^^ if the tuple conforms to (band, genre) structure,
+    // then let these be the constituent parts of this tuple -> so that it can be reused after =>
     case _ => "I don't know what you're talking about"
   }
 
   // decomposing lists
-  val aList = List(1,2,3)
+  val aList = List(1, 2, 3)
   val listDescription = aList match {
     case List(_, 2, _) => "List containing 2 on its second position"
+    //^^ 4 or 2 element pattern will not match the list! It should be 3 element long and 2nd element is 2!
     case _ => "unknown list"
+    // if PM doesn't match anything, it will throw a MatchError, that's why we normally add "case _"
+    // PM will try all cases in sequence, so don't put "case _" in the beginning!
   }
-
-  // if PM doesn't match anything, it will throw a MatchError
-  // PM will try all cases in sequence
 }
